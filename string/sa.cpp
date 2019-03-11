09aac788a564d8c506d36254dc2ac5d617b9a7c3
Strikeskids <github@strikeskids.com>
2019-03-11 14:35:35 -0400

    Add suffix array code

---
+++ string/sa.cpp
@@ -0,0 +1,39 @@
+int wa[maxn],wb[maxn],wv[maxn],ws[maxn];
+
+int sacmp(int *r,int a,int b,int l) {
+	return r[a]==r[b]&&r[a+l]==r[b+l];
+}
+
+// input: r: array of length n, 0 <= r_i < m, r[n] = -1
+// output: sa: suffix array (suffixes sorted lexicographically)
+void compute_sa(int n,int m,int *r,int *sa) {
+	int i,j,p,*x=wa,*y=wb,*t;
+	assert(n<maxn&&m<maxn);
+	for(i=0;i<n;++i) assert(r[i]<m);
+	for(i=0;i<m;i++) ws[i]=0;
+	for(i=0;i<n;i++) ws[x[i]=r[i]]++;
+	for(i=1;i<m;i++) ws[i]+=ws[i-1];
+	for(i=n-1;i>=0;i--) sa[--ws[x[i]]]=i;
+	for(j=1,p=1;p<n;j*=2,m=p) {
+		assert(j<n);
+		for(p=0,i=n-j;i<n;i++) y[p++]=i;
+		for(i=0;i<n;i++) if(sa[i]>=j) y[p++]=sa[i]-j;
+		for(i=0;i<n;i++) wv[i]=x[y[i]];
+		for(i=0;i<m;i++) ws[i]=0;
+		for(i=0;i<n;i++) ws[wv[i]]++;
+		for(i=1;i<m;i++) ws[i]+=ws[i-1];
+		for(i=n-1;i>=0;i--) sa[--ws[wv[i]]]=y[i];
+		for(t=x,x=y,y=t,p=1,x[sa[0]]=0,i=1;i<n;i++)
+			x[sa[i]]=sacmp(y,sa[i-1],sa[i],j)?p-1:p++;
+	}
+	sa[n] = n;
+}
+
+// input: r: array of length n, sa: suffix array, r[n] = -1
+// output: h: h[i] = lcp(sa[i],sa[i+1])
+void compute_lcp(int n,int *r,int *sa,int *h) {
+	int i,j,k=0;
+	for(i=0;i<n;i++) wa[sa[i]]=i;
+	for(i=0;i<n;h[wa[i++]]=k)
+		for(k?k--:0,j=sa[wa[i]+1];r[i+k]==r[j+k];k++) {}
+}
---
+++ string/test_sa.cpp
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+const int maxn = 1000;
+
+#include "sa.cpp"
+
+const char *CASES[] = {
+	"abacaba",
+	"abacadabra",
+	"cabaccbabcaacbbccbacbcbccbbaaababaabbcbcccabccacaccabccbaacaababcbcbbcbcbbababbccaacbabaacbccbcbaaca",
+	"bcaabbbccbcbbaabbbccaaccabcaacbacbbcabbabbbcabccaaabbabbccacbbabacbaabbbbacbcbabcccbbccbacbabaacaaab",
+	"bcacccbcbacbbccbbaababcccccaabbbbcaabaababcacaaaacccaacacaccccbacbaaacacacbbbbacbbbabbbabbccbabaccba",
+	"bcbaabccabaaaccacccccbabbaccacaacabcbbaaacbbcabaaaacabcccaaabbcccabcbaaaacaacacbbccccacbaaababbaacab",
+	"cbbbaacbcbaabbabacaaacbcacacbcccccbcbaacababababbbcabbaacaccabaabccbcabbaaaaaacbcbacbbbcbcbcbaaaabcb",
+	"ccbcbbbbabbacbcccaaabbabcaaaabbaaabaccbcaabababcbacbbbbaabaacacabacbbcbaccccccbbabcbbcbcbaaccbcbcbcb",
+	"bbcbbcbacccccaccbaccbcbabbbcccacaaccabcabcbacbbbacaabbbaaaaacabcbaaaaacacbacbbccabbacaacacabcabcbcbc",
+	"abaacbcbbcaacbacabccccacbcbaabaaababcbcbcbbcbacaccaaaaaaccbaabacbacaccbccacaaababbababababbbcabcacab",
+	"aabcacbabcbcbaaccaabbaaaaccbbcabaabacbbcbcbccacacbcacaaaccccbacaaacccabcbcaaaabbcbcaaabbbbbacccbccab",
+	"abbccaabaccabbaacccabbaabaacbcabbaccbbaccacccaababababccccccaabbaaabacbbcbcbccaaaccbaabcacbbcbbacbcb",
+	"abaaaabbbbacaaaabcaaaaccaaaaccbaccabcccbabcbbbcacbbaabbccbbaccbccccacbbcbbbbccbbcabacccaaacacabbacbc",
+	"cbbbaabbbaabbbaaabccbcabccaccccabaabbccaaccbabbaaabbcaabbbccbaaaacccbbbbacbababbbbaabcbcbcababccccba",
+	"bbccabbbcbbcbcbbabbbbbcccccaabbacacababbcabbccbcbcaaabaacccbaccacabbacbacaccbcbcbbccaccabbccbbabaaba",
+	"bbacaaabaabacbbbabaabaaaaaabacabbccbabaaaaabbcacabaabbacaacaaaccacbbbaccaacbabbacaacbbbcabcabbaaacbc",
+	"acbbbbabccabaabacaaaaabaabcbcbcbcbaaacabacaacaaacbabcabcccbccaaabbcccaabacabcaccabccbcaccacccaabbbca",
+	"bababbbaaccccaabbaccbcbbcbbabcbbbbbbccbaabccabcbcbcaabbcaabbccccaabbcbacccabaaacabbccacbbccabccbacca",
+	"cbcbaacabbcaaccbbabbbaababcabaacbcbcccacccccccbcbcabcaabbacbcaaaabacacbbcbacaccabbbabaaaaacbaaacccca",
+	"caaccbaacabbbcabcaabaaabcabcaaacbbacacbabccbccccaccbaccabbbcbcbaccccbabbaabbcaabbaccccabccccaababbab",
+	"bbcabbcbbbcbbbbabcccaabacbabbcbcaacbaabccbcccbaaabcccbbcbabcbbccbaaaccbcbabbbaaaaacbbbabcbccacbbaacb",
+	"cbbacbbcabaccbbaaabbbbbbcbbabcccbcbbbccbbcaccbbbcaaabaacaabaacbbabbcbbacabaabaabcbcbbaaabcbbababbbca",
+	"acacbbcccaababcbccbccabcacbaaccaabaababcacaabcabbcbabcaabbbacacbaacbbaacbccbacbabcbabacaaaaabacbcaac",
+	"accccaabcaccbcbbcbcabacabbbbcccbccbbbaaaabbbaaacacaccbaccccbcaabbbbbacaccaabbcbacbbbacbbcccbcbbacbca",
+	"cccbcbbcabcbbaaacbaabccaaabcbbaaacacccbaccccccacccacabbcbaacaaaaaaacacbcbaccbacabbabaaababaaaaabcabc",
+};
+
+int str[maxn];
+int sa[maxn];
+int lcp[maxn];
+
+bool suffixlt(int n, int *r, int a, int b) {
+	for (int i = 0; i + a < n && i + b < n; ++i) {
+		if (r[a+i] < r[b+i]) return true;
+		else if (r[b+i] < r[a+i]) return false;
+	}
+	return a > b;
+}
+
+void check_sa(int n, int *r, int *sa) {
+	for (int i = 1; i < n; ++i) {
+		assert(suffixlt(n, r, sa[i-1], sa[i]));
+	}
+}
+
+void check_lcp(int n, int *r, int *sa, int *lcp) {
+	for (int i = 1; i < n; ++i) {
+		int a = sa[i-1], b = sa[i];
+		int k = lcp[i-1];
+		assert(a + k <= n && b + k <= n);
+		assert(a + k == n || b + k == n || r[a+k] != r[b+k]);
+		while (k-- > 0) assert(r[a+k]==r[b+k]);
+	}
+}
+
+void print_sa(int n, const char *s, const int *sa) {
+	for (int i = 0 ; i < n; ++i) {
+		printf("%s\n", s+sa[i]);
+	}
+}
+
+int main() {
+	for (const char *c : CASES) {
+		int n = 0;
+		for (; c[n]; ++n) { str[n] = c[n] - 'a'; }
+		str[n] = -1;
+		compute_sa(n, 26, str, sa);
+		check_sa(n, str, sa);
+		compute_lcp(n, str, sa, lcp);
+		check_lcp(n, str, sa, lcp);
+	}
+}
